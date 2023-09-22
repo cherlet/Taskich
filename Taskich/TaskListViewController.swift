4ae519d5ffd955a15c47712307ec7a19f1989b73
justUzm <uzmel@icloud.com>
2023-09-22 21:34:50 +0300

    Removed VC(UIVC). All logic was transferred to TaskListViewController (UITableVC)

--- Taskich/TaskListViewController.swift
+++ Taskich/TaskListViewController.swift
@@ -9,31 +9,25 @@ import UIKit
 
 class TaskListViewController: UITableViewController {
 
+    var tasks = Task.makeTasks()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(TaskCell.self, forCellReuseIdentifier: "TaskCell")
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tasks.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as? TaskCell else { fatalError() }
 
-        // Configure the cell...
+        cell.configure(task: tasks[indexPath.row])
 
         return cell
     }
-    */
-
 }
